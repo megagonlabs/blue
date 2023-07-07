a497cb6ed984d34c6bc28b862e198f8784a26221
Eser Kandogan <eser@megagon.ai>
2023-07-07 15:01:52 -0700

    add missing mkdir

--- agents/simple_user/docker_build_agent.sh
+++ agents/simple_user/docker_build_agent.sh
@@ -2,7 +2,7 @@
 echo 'Building...'
 
 # copy from shared lib
-cp -r ../shared_lib/* lib/shared/
+mkdir -p lib/shared & cp -r ../shared_lib/* lib/shared/
 
 # build docker
 docker build -t blue-agent-simple_user:latest -f Dockerfile.agent .
