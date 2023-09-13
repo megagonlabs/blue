2fca3c2591ea4bf61aea0e9fdf491d660d07b78c
Kushan <kushan@megagon.ai>
2023-09-13 13:15:41 -0700

    kg_agent

---
+++ agents/knowledge_grounding/src/knowledge_grounding_agent.py
@@ -0,0 +1,132 @@
+###### OS / Systems
+import os
+import sys
+
+###### Add lib path
+sys.path.append('./lib/')
+sys.path.append('./lib/shared/')
+
+###### 
+import time
+import argparse
+import logging
+import time
+import uuid
+import random
+# import openai
+
+###### Parsers, Formats, Utils
+import re
+import csv
+import json
+
+import itertools
+from tqdm import tqdm
+
+###### Blue
+from agent import Agent
+from session import Session
+
+from py2neo import Graph
+
+# set log level
+logging.getLogger().setLevel(logging.INFO)
+
+#######################
+class KnowledgGroundingAgent(Agent):
+    def __init__(self, session=None, input_stream=None, processor=None, properties={}):
+        super().__init__("KNOWLEDGE", session=session, input_stream=input_stream, processor=processor, properties=properties)
+
+    def _initialize_properties(self):
+        super()._initialize_properties()
+
+        listeners = {}
+        self.properties['listens'] = listeners
+        listeners['includes'] = ['RECORDER']
+        listeners['excludes'] = [self.name]
+
+        # rationalizer config
+        self.properties['requires'] = ['title', 'title_recommendation']
+
+    def default_processor(self, stream, id, label, data, dtype=None, tags=None, properties=None, worker=None):    
+        if label == 'EOS':
+            pass
+                
+        elif label == 'BOS':
+            pass
+        elif label == 'DATA':
+            # check if a required variable is seen
+            requires = properties['requires']
+
+            required_recorded = True
+            for require in requires:
+                # check if require is in session memory
+                if worker:
+                    v = worker.get_session_data(require)
+                    logging.info("variable: {} value: {}".format(require, v))
+                    if v is None:
+                        required_recorded = False 
+                        break
+
+            if required_recorded:
+                if worker:
+                    graph = Graph("http://18.216.233.236:7474", auth=("neo4j", "444@Castro"))
+                    person = "Eser Kandogan"
+                    next_title = "Software Engineer - 2"
+
+                    name_query = '''
+                        MATCH (p:PERSON{{name: '{}'}})-[h1:HAS]->(b)-[h2:HAS]->(c)
+                        RETURN c.label, h2.duration
+                        ORDER BY h2.duration DESC
+                        LIMIT 2
+                    '''.format(person)
+
+                    title_query = '''
+                        MATCH (j:TITLE{{label: '{}'}})-[r:REQUIRES]->(b)
+                        RETURN b.label, r.avg_duration
+                        ORDER BY r.avg_duration DESC
+                        LIMIT 2
+                    '''.format(next_title)
+
+                    s1 = graph.run(name_query) #.to_ndarray(dtype='str')
+                    s2 = graph.run(title_query) #.to_ndarray(dtype='str')
+                    print(s1, s2)
+                    return "1"
+    
+        return None
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--session', type=str)
+    parser.add_argument('--input_stream', type=str)
+    parser.add_argument('--properties', type=str)
+ 
+    args = parser.parse_args()
+
+    session = None
+    a = None
+
+    # set properties
+    properties = {}
+    p = args.properties
+    if p:
+        # decode json
+        properties = json.loads(p)
+
+    if args.session:
+        # join an existing session
+        session = Session(args.session)
+        a = KnowledgGroundingAgent(session=session, properties=properties)
+    elif args.input_stream:
+        # no session, work on a single input stream
+        a = KnowledgGroundingAgent(input_stream=args.input_stream, properties=properties)
+    else:
+        # create a new session
+        a = KnowledgGroundingAgent(properties=properties)
+        a.start_session()
+
+    # wait for session
+    session.wait()
+
+
+
---
+++ agents/knowledge_grounding/src/testing_kg.py
@@ -0,0 +1,27 @@
+from py2neo import Graph
+
+def main():
+    graph = Graph("http://18.216.233.236:7474", auth=("neo4j", "444@Castro"))
+    person = "Eser Kandogan"
+    next_title = "Software Engineer - 2"
+
+    name_query = '''
+        MATCH (p:PERSON{{name: '{}'}})-[h1:HAS]->(b)-[h2:HAS]->(c)
+        RETURN c.label, h2.duration
+        ORDER BY h2.duration DESC
+        LIMIT 2
+    '''.format(person)
+
+    title_query = '''
+        MATCH (j:TITLE{{label: '{}'}})-[r:REQUIRES]->(b)
+        RETURN b.label, r.avg_duration
+        ORDER BY r.avg_duration DESC
+        LIMIT 2
+    '''.format(next_title)
+
+    s1 = graph.run(name_query) #.to_ndarray(dtype='str')
+    s2 = graph.run(title_query) #.to_ndarray(dtype='str')
+    
+
+if __name__ == '__main__':
+    main()
