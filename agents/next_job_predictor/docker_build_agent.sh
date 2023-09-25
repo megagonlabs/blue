73e9a6f04c3207670b7625998e8de9db22527608
Eser Kandogan <eser@megagon.ai>
2023-09-25 14:02:41 -0700

    remove obsolete code

--- agents/next_job_predictor/docker_build_agent.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-echo 'Building...'
-
-# copy from shared lib
-mkdir -p lib/shared & cp -r ../shared_lib/* lib/shared/
-
-# build docker
-docker build -t blue-agent-simple_counter:latest -f Dockerfile.agent .
-echo 'Done...'
