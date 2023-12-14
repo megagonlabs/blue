5380798dcf85cfb8c1b17160e91843c648ddba92
Eser Kandogan <eser@megagon.ai>
2023-12-14 09:29:56 -0800

    agents lib, conjuctions on listeners

---
+++ agents/lib/agent.py
@@ -0,0 +1,357 @@
+###### OS / Systems
+from curses import noecho
+import os
+import sys
+
+###### 
+import time
+import argparse
+import logging
+import time
+import uuid
+import random
+
+###### Parsers, Formats, Utils
+import re
+import csv
+import json
+
+
+import itertools
+from tqdm import tqdm
+
+###### Backend, Databases
+import redis
+
+
+###### Blue
+from producer import Producer
+from consumer import Consumer
+from session import Session
+from worker import Worker
+
+class Agent():
+    def __init__(self, name, session=None, input_stream=None, processor=None, properties={}):
+
+        self.name = name
+
+        self._initialize(properties=properties)
+
+        # override, if necessary
+        if processor is not None:
+            self.processor = lambda *args, **kwargs: processor(*args, **kwargs, properties=self.properties)
+        else:
+            self.processor = lambda *args, **kwargs: self.default_processor(*args, **kwargs, properties=self.properties)
+
+        self.input_stream = input_stream
+        
+        self.set_session(session)
+
+        self.aggregate_producer_id = None
+        self.consumer = None
+
+        self.workers = []
+
+        self._start()
+
+    ###### initialization
+    def _initialize(self, properties=None):
+        self._initialize_properties()
+        self._update_properties(properties=properties)
+
+
+    def _initialize_properties(self):
+        self.properties = {}
+
+        # db connectivity
+        self.properties['host'] = 'localhost'
+        self.properties['port'] = 6379
+
+        # aggregator (have a single producer for all workers)
+        self.properties['aggregator'] = False
+        self.properties['aggregator.eos'] = 'FIRST'
+
+        ### include/exclude list of rules to listen to agents/tags
+        listeners = {}
+        self.properties['listens'] = listeners
+        listeners['includes'] = ['.*']
+        listeners['excludes'] = [self.name]
+
+        ### default tags to tag output streams
+        tags = []
+        self.properties['tags'] = tags
+
+
+    def _update_properties(self, properties=None):
+        if properties is None:
+            return
+
+        # override
+        for p in properties:
+            self.properties[p] = properties[p]
+
+    ###### database, data
+    def _start_connection(self):
+        host = self.properties['host']
+        port = self.properties['port']
+
+        self.connection = redis.Redis(host=host, port=port, decode_responses=True)
+
+    ###### worker
+    def create_worker(self, input_stream, tags=None, id=None):
+        if self.properties['aggregator']:
+            # generate unique id for aggregate producer
+            if self.aggregate_producer_id is None:
+                self.aggregate_producer_id = self.name + ":" + str(hex(uuid.uuid4().fields[0]))[2:]
+            # if id not set use aggregate_producer
+            if id is None:
+                id = self.aggregate_producer_id
+        
+        worker = Worker(self.name, input_stream, agent=self, id=id, processor=lambda *args, **kwargs: self.processor(*args, **kwargs, tags=tags), session=self.session, properties=self.properties)
+      
+        return worker
+
+    ###### default processor, override
+    def default_processor(self, stream, id, label, data, dtype=None, tags=None, properties=None, worker=None):
+        logging.info('default processor: override')
+        logging.info(stream)
+        logging.info(tags)
+        logging.info(id)
+        logging.info(label)
+        logging.info(data)
+        logging.info(properties)
+        logging.info(worker)
+
+    ###### sesion
+    def start_session(self):
+        # create a new session
+        session = Session()
+        
+        # set agent's session, start listening...
+        self.set_session(session)
+
+        # start consuming session stream
+        self._start_session_consumer()
+        return session
+
+
+    def set_session(self, session):
+        self.session = session
+
+        if self.session:
+           self.session.add_agent(self)
+
+
+
+    def session_listener(self, id, message):   
+        # listen to session stream
+       
+        label = message['label']
+       
+        if label == 'ADD':
+            data = json.loads(message['data'])
+
+            input_stream = data['stream']
+            tags = data['tags']
+
+            # agent define what to listen to using include/exclude expressions
+            matches = self._match_listen_to_tags(tags)
+            if len(matches) == 0:
+                logging.info("Not listening to {stream} with {tags}...".format(stream=input_stream, tags=tags))
+                return 
+
+            logging.info("Spawning worker for stream {stream} with matching tags {matches}...".format(stream=input_stream, matches=matches))
+            session_stream = self.session.get_stream()
+
+            # create and start worker
+            # TODO; pass tag to worker, with parameters
+            worker = self.create_worker(input_stream, tags=matches)
+            self.workers.append(worker)
+            
+            logging.info("Spawned worker for stream {stream}...".format(stream=input_stream))
+
+    # def _match_listen_to_tags(self, tags):
+    #     matches = []
+    #     # process includes for each tag
+    #     for tag in tags:
+
+    #         includes = self.properties['listens']['includes']
+    #         for i in includes:
+    #             p = re.compile(i)
+    #             if p.match(tag):
+    #                 matches.append(tag)
+        
+
+    #     if len(matches) == 0:
+    #         return matches
+
+    #     # process excludes for each tag
+    #     for tag in tags:
+
+    #         excludes = self.properties['listens']['excludes']
+    #         for x in excludes:
+    #             p = re.compile(x)
+    #             if p.match(tag):
+    #                 logging.info("Matched exclude rule: {rule}".format(rule=x))
+    #                 return []
+       
+    #     return matches
+
+    def _match_listen_to_tags(self, tags):
+        matches = set()
+
+        includes = self.properties['listens']['includes']
+        excludes = self.properties['listens']['excludes']
+
+        for i in includes:
+            p = None
+            if type(i) == str:
+                p = re.compile(i)
+                for tag in tags:
+                    if p.match(tag):
+                        matches.add(tag)
+                        logging.info("Matched include rule: {rule}".format(rule=str(i)))
+            elif type(i) == list:
+                m = set()
+                a = True
+                for ii in i:
+                    p = re.compile(ii)
+                    b = False
+                    for tag in tags:
+                        if p.match(tag):
+                            m.add(tag)
+                            b = True
+                            break
+                    if b:
+                        continue
+                    else:
+                        a = False
+                        break
+                if a:
+                    matches = matches.union(m)
+                    logging.info("Matched include rule: {rule}".format(rule=str(i)))
+
+        if len(matches) == 0:
+            return list(matches)
+
+        for x in excludes:
+            p = None
+            if type(x) == str:
+                p = re.compile(x)
+                if p.match(tag):
+                    logging.info("Matched exclude rule: {rule}".format(rule=str(x)))
+                    return []
+            elif type(x) == list:
+                a = True
+                if len(x) == 0:
+                    a = False
+                for xi in x:
+                    p = re.compile(xi)
+                    b = False
+                    for tag in tags:
+                        if p.match(tag):
+                            b = True
+                            break
+                    if b:
+                        continue
+                    else:
+                        a = False
+                        break
+                if a:
+                    logging.info("Matched exclude rule: {rule}".format(rule=str(x)))
+                    return []
+
+        return list(matches)
+
+    def interact(self, data):
+        if self.session is None:
+            self.start_session()
+
+        # create worker to emit data for session
+        worker = self.create_worker(None)
+
+        # write data, automatically notify session on BOS
+        worker.write(data)
+
+    def _start(self):
+        self._start_connection()
+        
+        # logging.info('Starting agent {name}'.format(name=self.name))
+
+        # if agent is associated with a session
+        if self.session:
+            self._start_session_consumer()
+        # else if agent is dedicated to an input stream 
+        elif self.input_stream:
+            # create and start worker
+            worker = self.create_worker(self.input_stream)
+            self.workers.append(worker)
+
+        logging.info('Started agent {name}'.format(name=self.name))
+
+
+    def _start_session_consumer(self):
+        # start a consumer to listen to session stream
+        if self.session:
+            session_stream = self.session.get_stream()
+
+            if session_stream:
+                self.consumer = Consumer(self.name, session_stream, listener=lambda id, message : self.session_listener(id, message), properties=self.properties)
+                self.consumer.start()
+
+
+    def stop(self):
+        # send stop to each worker
+        for w in self.workers:
+            w.stop()
+
+    def wait(self):
+        # send wait to each worker
+        for w in self.workers:
+            w.wait()
+
+
+
+#######################
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--name', type=str, default='processor')
+    parser.add_argument('--input_stream', type=str, default='input')
+    parser.add_argument('--properties', type=str)
+    parser.add_argument('--loglevel', default="INFO", type=str)
+ 
+    args = parser.parse_args()
+   
+    # set logging
+    logging.getLogger().setLevel(args.loglevel.upper())
+
+
+    # set properties
+    properties = {}
+    p = args.properties
+    if p:
+        # decode json
+        properties = json.loads(p)
+    
+
+    # sample func to process data from 
+    # return a value other than None
+    # to create a stream 
+    def processor(stream, id, label, data, dtype=None):
+        logging.into(stream)
+        logging.info(id)
+        logging.info(label)
+        logging.info(data)
+       
+        return None
+
+
+    # create an agent and then create a session, and add agents
+    a = Agent(args.name, processor=processor, session=None, properties=properties)
+    s = a.start_session()
+
+    # optionally you can create an agent in a session directly
+    b = Agent(args.name, processor=processor, session=s, properties=properties)
+
+  
+   
---
+++ agents/lib/consumer.py
@@ -0,0 +1,234 @@
+###### OS / Systems
+import os
+import sys
+
+###### 
+import time
+import argparse
+import logging
+import time
+import uuid
+import random
+
+###### Parsers, Formats, Utils
+import re
+import csv
+import json
+
+import itertools
+from tqdm import tqdm
+
+###### Backend, Databases
+import redis
+
+###### Threads
+import threading
+import concurrent.futures
+
+# set log level
+logging.getLogger().setLevel(logging.INFO)
+
+
+class Consumer():
+    def __init__(self, name, stream, gid=None, listener=None, properties={}):
+
+        self.name = name
+        self.stream = stream
+
+        self._initialize(properties=properties)
+        
+        if gid is None:
+            gid = hex(uuid.uuid4().fields[0])[2:]
+        self.gid = gid 
+
+        self.group = self.name + ":" + str(self.gid)
+
+        if listener is None:
+            listener = lambda id, message: print("{id}:{message}".format(id=id, message=message))
+
+        self.listener = listener
+
+        self.threads = []
+
+        
+
+    ###### initialization
+    def _initialize(self, properties=None):
+        self._initialize_properties()
+        self._update_properties(properties=properties)
+
+    def _initialize_properties(self):
+        self.properties = {}
+        self.properties['num_threads'] = 1
+        self.properties['host'] = 'localhost'
+        self.properties['port'] = 6379
+
+    def _update_properties(self, properties=None):
+        if properties is None:
+            return
+
+        # override
+        for p in properties:
+            self.properties[p] = properties[p]
+
+    ####### open connection, create group, start threads
+    def start(self):
+
+        # logging.info("Starting consumer {c} for stream {s}".format(c=self.name,s=self.stream))
+        self.stop_signal = False
+
+        self._start_connection()
+
+        self._start_group()
+
+        self._start_threads()
+
+        logging.info("Started consumer {c} for stream {s}".format(c=self.name,s=self.stream))
+
+    def stop(self):
+        logging.info("Stopping consumer {c} for stream {s}".format(c=self.name,s=self.stream))
+        self.stop_signal = True
+
+    def wait(self):
+        for t in self.threads:
+            t.join()
+
+    def _start_connection(self):
+        host = self.properties['host']
+        port = self.properties['port']
+
+        self.connection = redis.Redis(host=host, port=port, decode_responses=True)
+
+    def _start_group(self):
+        # create group if it doesn't exists, print group info 
+        s = self.stream
+        g = self.group
+        r = self.connection
+       
+        try:
+            # logging.info("Creating group {g}...".format(g=g))
+            r.xgroup_create(name=s, groupname=g, id=0)
+        except:
+            logging.info("Group {g} exists...".format(g=g))
+
+        # self._print_group_info()
+
+    def _print_group_info(self):
+        s = self.stream
+        g = self.group
+        r = self.connection
+
+        logging.info("Group info for stream {s}".format(s=s))
+        res = r.xinfo_groups(name=s)
+        for i in res:
+            logging.info(f"{s} -> group name: {i['name']} with {i['consumers']} consumers and {i['last-delivered-id']}"
+                + f" as last read id")
+
+    def get_stream(self):
+        return self.stream
+
+    def get_group(self):
+        return self.group
+
+    def _consume_stream(self, c):
+        s = self.stream
+        g = self.group
+        r = self.connection
+        l = self.listener
+
+        logging.info("[Thread {c}]: starting".format(c=c))
+        while(True):
+            
+            if self.stop_signal:
+                break
+            
+            # check any pending, if so claim
+            m = r.xautoclaim(count=1, name=s, groupname=g, consumername=str(c), min_idle_time=10000, justid=False)
+    
+            if len(m) > 0:
+                d = m 
+                id = d[0]
+                message = d[1]
+
+                # check special token (no data to claim)
+                if id == "0-0":
+                    pass
+                else:
+                    logging.info("[Thread {c}]: reclaiming... {s} {id}".format(c=c, s=s, id=id))
+
+                    # listen
+                    l(id, message)
+
+                    # ack
+                    r.xack(s, g, id)
+                    continue
+
+            # otherwise read new
+            m = r.xreadgroup(count=1, streams={s:'>'}, block=200, groupname=g, consumername=str(c))
+            
+            if len(m) > 0:
+                e = m[0]
+                s = e[0]
+                d = e[1][0]
+                id = d[0]
+                message = d[1]
+
+                logging.info("[Thread {c}]: listening... stream:{s} id:{id} message:{message}".format(c=c, s=s, id=id, message=message))
+                
+                # listen
+                l(id, message)
+
+                # occasionally throw exception (for testing failed threads)
+                # if random.random() > 0.5:
+                #    print("[Thread {c}]: throwing exception".format(c=c))
+                #    raise Exception("exception")          
+                
+                # ack 
+                r.xack(s, g, id)
+
+        logging.info("[Thread {c}]: finished".format(c=c))
+
+    
+    def _start_threads(self):
+        # start threads
+        num_threads = self.properties['num_threads']
+        
+        for i in range(num_threads):
+            t = threading.Thread(target=lambda : self._consume_stream(self.name + "-" + str(i)), daemon=True)
+            t.start()
+            self.threads.append(t)
+            
+
+    def _delete_stream(self):
+        s = self.stream
+        r = self.connection
+
+        l = r.xread(streams={s:0})
+        for _, m in l:
+            [ r.xdel( s, i[0] ) for i in m ]
+
+
+
+
+#######################
+if __name__ == "__main__":
+   parser = argparse.ArgumentParser()
+   parser.add_argument('--name', type=str, default='consumer')
+   parser.add_argument('--stream', type=str, default='stream')
+   parser.add_argument('--threads', type=int, default=1)
+   parser.add_argument('--loglevel', default="INFO", type=str)
+ 
+   args = parser.parse_args()
+   
+   # set logging
+   logging.getLogger().setLevel(args.loglevel.upper())
+
+
+   # create a listener
+   def listener(id, message):
+       print("hello {message}".format(message=message))
+
+   c = Consumer(args.name, args.stream, listener=listener)
+   c.start()
+  
+
