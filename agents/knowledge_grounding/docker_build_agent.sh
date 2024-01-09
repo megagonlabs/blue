e7b6d38d83608ea952d8df47a9fc4b9702b947a1
Kushan <kushan@megagon.ai>
2024-01-09 15:49:14 -0800

    Data registry from KG agent

--- agents/knowledge_grounding/docker_build_agent.sh
+++ agents/knowledge_grounding/docker_build_agent.sh
@@ -7,6 +7,10 @@ mkdir -p lib/agent; cp -r ../lib/* lib/agent/
 # copy from utils shared lib
 mkdir -p lib/utils; cp -r ../../utils/lib/* lib/utils/
 
+mkdir -p lib/data_registry; cp -r ../../platform/data_registry/src/* lib/data_registry
+
+cp -r ../../platform/lib/* lib/data_registry
+
 # build docker
 docker build -t blue-agent-knowledge_grounding:latest -f Dockerfile.agent .
 echo 'Done...'
