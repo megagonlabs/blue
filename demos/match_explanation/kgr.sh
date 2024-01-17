39e170515c68c3fb6dfea6fae4903353d6df9b8a
Sajjadur Rahman <rahmansunny071@gmail.com>
2024-01-17 13:47:26 -0800

    KGD able to connect to localhost via docker and stream doc query and insight query

--- demos/match_explanation/kgr.sh
+++ demos/match_explanation/kgr.sh
@@ -8,7 +8,7 @@ echo $1
 echo $2
 echo $3
 echo $4
-docker run -e profile="$1" -e title="$2" -e next_title="$3" -e session=$4 --network="host" --name blue_agent_knowledge_grounding -d blue-agent-knowledge_grounding:latest
+docker run -e profile="$1" -e title="$2" -e next_title="$3" -e session=$4 -e NEO4J_USER=$NEO4J_USER -e NEO4J_PWD=$NEO4J_PWD --network="host" --name blue_agent_knowledge_grounding -d blue-agent-knowledge_grounding:latest
 # Rationalizer Agent
 echo "Bring up RATIONALIZER OPENAI agent:"
 #docker run -e session=$1 -d --network="host" --name blue_agent_rationalizer blue-agent-rationalizer:latest
