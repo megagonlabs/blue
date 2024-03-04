6f1f96b041538ac07b1c7e2263cc7d0fa9190715
rafaellichen <rafaellichen@gmail.com>
2024-03-04 11:11:46 -0800

    update docker_build_frontend.sh to take in icon package token for npm install #60

--- platform/frontend/docker_build_frontend.sh
+++ platform/frontend/docker_build_frontend.sh
@@ -1,7 +1,14 @@
 #/bin/bash
 echo 'Building Platform Frontend...'
 
+fa_token=$1
+# if no argument supplied
+if [ -z "$fa_token" ]
+    then
+        read -p "Font Awesome Pro Package Token: " fa_token
+fi
+
 # build docker
-docker build -t blue-platform-frontend:latest -f Dockerfile.frontend .
+docker build . --tag blue-platform-frontend:latest -f Dockerfile.frontend --build-arg fa_token=$fa_token
 
 echo 'Done...'
