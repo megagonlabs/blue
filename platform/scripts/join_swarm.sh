3cc8b47f6c2f92b59bbeb5dd97a5dec4155a721c
Ubuntu <ubuntu@ip-10-0-160-75.ec2.internal>
2024-04-04 18:56:32 +0000

    fix swarm scripts

--- platform/scripts/join_swarm.sh
+++ platform/scripts/join_swarm.sh
@@ -1,11 +1,12 @@
 #/bin/bash
 
-# USAGE: join_swarm worker|manager
+# USAGE: join_swarm worker|manager <SWARM_DIRECTORY>
 # worker|manager: worker or manager type
+# SWARM_DIRECTORY: directory to save/retrieve tokens
 
 # Extract saved manager ip and token, based on node type
-IP_ADDRESS=$(cat .manager.ip)
-TOKEN=$(cat .$1.token)
+IP_ADDRESS=$(cat $2/.manager.ip)
+TOKEN=$(cat $2/.$1.token)
 
 echo "Joining swarm on $IP_ADDRESS as $1"
 docker swarm join --token $TOKEN $IP_ADDRESS:2377
