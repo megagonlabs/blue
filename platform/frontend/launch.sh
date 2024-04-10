cf9ee258bb099d7615c4929cc89382b570108f06
rafaellichen <rafaellichen@gmail.com>
2024-04-10 11:17:18 -0700

    update launch.sh to write NEXT_PUBLIC_WS_SERVER/BLUE_PUBLIC_WS_SERVER

--- platform/frontend/launch.sh
+++ platform/frontend/launch.sh
@@ -1,4 +1,5 @@
 #/bin/sh
 echo "NEXT_PUBLIC_API_SERVER=$BLUE_PUBLIC_API_SERVER" > .env.production
+echo "NEXT_PUBLIC_WS_SERVER=$BLUE_PUBLIC_WS_SERVER" >> .env.production
 npm run build
 npm run start
