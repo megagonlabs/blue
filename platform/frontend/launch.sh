4bfa2f1a345c25bebf90f68e1ae1db60e7cc17ac
Eser Kandogan <eser@megagon.ai>
2024-04-10 23:51:14 +0000

    fix

--- platform/frontend/launch.sh
+++ platform/frontend/launch.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 echo "NEXT_PUBLIC_REST_API_SERVER=http://${BLUE_PUBLIC_API_SERVER}" > .env.production
-echo "NEXT_PUBLIC_WS_SERVER=ws://${BLUE_PUBLIC_WS_SERVER}" >> .env.production
+echo "NEXT_PUBLIC_WS_SERVER=ws://${BLUE_PUBLIC_API_SERVER}" >> .env.production
 npm run build
 npm run start
