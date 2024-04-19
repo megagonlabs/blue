e780ba1d4f0a2169d14fa7e9cb0b8011e610819e
rafaellichen <rafaellichen@gmail.com>
2024-04-19 11:57:41 -0700

    draft

---
+++ platform/cli/web-auth/next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    output: "export",
+    distDir: "../blue/web/auth/out",
+};
+export default nextConfig;
