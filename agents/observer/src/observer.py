7aac38ff1389ee3b11b4ac232c8ba1ac4d11a16c
rafaellichen <rafaellichen@gmail.com>
2024-05-16 10:40:23 -0700

    add timestamp to ovserver session message for UI to show the order of messages correctly

--- agents/observer/src/observer.py
+++ agents/observer/src/observer.py
@@ -115,6 +115,7 @@ class ObserverAgent(Agent):
                                 "content": value,
                             },
                             "stream": stream,
+                            "timestamp": time.time(),
                         }
                     )
                 )
