845294a385971834be0b14d9241b692d46198820
Eser Kandogan <eser@megagon.ai>
2024-06-12 16:02:02 +0000

    add build script for api

--- platform/api/docker_build_api.sh
+++ platform/api/docker_build_api.sh
@@ -1,24 +1,7 @@
 #/bin/bash
-echo 'Building Platform API...'
+source $(dirname $0)/build_agent.sh
 
-# cleanup libs
-rm -r lib
-
-# copy from platform shared lib
-mkdir -p lib/platform; cp -r ../lib/* lib/platform/
-
-# copy from agent shared lib
-mkdir -p lib/agents; cp -r ../../agents/lib/* lib/agents/
-mkdir -p lib/agents; cp -r ../../agents/observer/src/* lib/agents/
-
-# copy from agent_registry shared lib
-mkdir -p lib/agent_registry; cp -r ../agent_registry/src/* lib/agent_registry/
-
-# copy from data_registry shared lib
-mkdir -p lib/data_registry; cp -r ../data_registry/src/* lib/data_registry/
-
-# copy from utils shared lib
-mkdir -p lib/utils; cp -r ../../utils/lib/* lib/utils/
+echo 'Building docker image...'
 
 # build docker
 docker build -t blue-platform-api:latest -f Dockerfile.api .
