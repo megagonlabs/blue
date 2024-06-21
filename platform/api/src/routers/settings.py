0be39bbccedc1b72d83d4581a88102d35e8dd604
Eser Kandogan <eser@megagon.ai>
2024-06-21 15:42:04 +0000

    deploy dev

--- platform/api/src/routers/settings.py
+++ platform/api/src/routers/settings.py
@@ -2,15 +2,13 @@
 import os
 import sys
 
-##### 
-DEVELOPMENT = os.getenv("DEVELOPMENT", "False").lower() == "true"
-SECURE_COOKIE = os.getenv("SECURE_COOKIE", "True").lower() == "true"
 
 ###### Properties
 PROPERTIES = {}
 PROPERTIES["platform.name"] = os.getenv("BLUE_DEPLOY_PLATFORM")
 PROPERTIES["platform.deploy.version"] = os.getenv("BLUE_DEPLOY_VERSION")
 PROPERTIES["platform.deploy.target"] = os.getenv("BLUE_DEPLOY_TARGET")
+PROPERTIES["platform.deploy.secure"] = os.getenv("BLUE_DEPLOY_SECURE")
 PROPERTIES["api.server"] = os.getenv("BLUE_PUBLIC_API_SERVER")
 PROPERTIES["api.server.port"] = os.getenv("BLUE_PUBLIC_API_SERVER_PORT")
 PROPERTIES["api.server.protocol"] = os.getenv("BLUE_PUBLIC_API_SERVER_PROTOCOL")
@@ -21,3 +19,7 @@ PROPERTIES["data_registry.name"] = os.getenv("BLUE_DATA_REGISTRY")
 PROPERTIES["embeddings_model"] = os.getenv("BLUE_AGENT_REGISTRY_MODEL")
 
 PROPERTIES["db.host"] = "blue_db_redis"
+
+##### Other Settings
+DEVELOPMENT = os.getenv("BLUE_DEPLOY_DEVELOPMENT", "False").lower() == "true"
+SECURE_COOKIE = os.getenv("BLUE_DEPLOY_SECURE", "True").lower() == "true"
