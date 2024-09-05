c786c5d70c6fa68338606fccf2f47e8b89862084
Eser Kandogan <eser@megagon.ai>
2024-09-05 04:31:39 +0000

    fix build

--- agents/coordinator/build_agent.sh
+++ agents/coordinator/build_agent.sh
@@ -12,6 +12,9 @@ mkdir -p lib/agent; cp -r ../lib/* lib/agent/
 # copy from platform lib
 mkdir -p lib/platform; cp -r ../../platform/lib/* lib/platform/
 
+# copy from data_planner lib
+mkdir -p lib/data_planner; cp -r ../../platform/data_planner/src/* lib/data_planner/
+
 # copy from utils shared lib
 mkdir -p lib/utils; cp -r ../../utils/lib/* lib/utils/
 
