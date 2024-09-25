d008a8ec7240aad6916b070bbfb0cea82d21e75b
rafaellichen <rafaellichen@gmail.com>
2024-09-25 11:38:55 -0700

    unread session bell icon cursor initial

--- platform/frontend/src/pages/sessions.js
+++ platform/frontend/src/pages/sessions.js
@@ -370,6 +370,7 @@ export default function Sessions() {
                                 }`}
                             >
                                 <Button
+                                    style={{ cursor: "initial" }}
                                     large
                                     minimal
                                     icon={faIcon({ icon: faBell })}
