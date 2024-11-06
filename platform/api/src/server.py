8e7425710ff67e01ea97bd23dded5f2cf91ecd6b
rafaellichen <rafaellichen@gmail.com>
2024-11-06 12:02:17 -0800

    add 401 status for when accountId is absent

--- platform/api/src/server.py
+++ platform/api/src/server.py
@@ -143,7 +143,10 @@ async def session_verification(request: Request, call_next):
         # when authentication is disabled: upper layer needs to handle all identity and access verifications
         # all requests here are operating under administrator role
         json_payload = await request.json()
-        request.state.user = {'uid': pydash.objects.get(json_payload, 'accountId', None), 'role': 'admin'}
+        uid = pydash.objects.get(json_payload, 'accountId', None)
+        if pydash.is_empty(uid):
+            return JSONResponse(status_code=401, content={"message": "Account ID is unavailable"})
+        request.state.user = {'uid': uid, 'role': 'admin'}
         return await call_next(request)
 
 
