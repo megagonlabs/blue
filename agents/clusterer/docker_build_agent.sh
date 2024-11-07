1ae21d40f56b67dc95dfa9811f6d7795aa208581
Jackson Hassell <jackson@ip-10-117-185-214.us-east-2.compute.internal>
2024-11-07 21:28:09 +0000

    Initial class creation

---
+++ agents/clusterer/docker_build_agent.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+source $(dirname $0)/build_agent.sh
+
+echo 'Building docker image...'
+
+# build docker
+docker build -t blue-agent-clusterer:latest -f Dockerfile.agent .
+
+# tag image
+docker tag blue-agent-clusterer:latest blue-agent-clusterer:$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short HEAD)
+
+echo 'Done...'
