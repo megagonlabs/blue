a640553304499c4a2b57e3e7888fcb590a0999da
rafaellichen <rafaellichen@gmail.com>
2024-11-26 14:00:49 -0800

    small change

--- platform/frontend/src/components/jsonforms/renderers/DateTime.js
+++ platform/frontend/src/components/jsonforms/renderers/DateTime.js
@@ -1,7 +1,7 @@
 import { isDateTimeControl, rankWith } from "@jsonforms/core";
 import { withJsonFormsControlProps } from "@jsonforms/react";
 const DateTimeRenderer = () => {
-    return <div>date-time: not implemented</div>;
+    return <div>datetime: not implemented</div>;
 };
 export default withJsonFormsControlProps(DateTimeRenderer);
 export const DateTimeTester = rankWith(3, isDateTimeControl);
