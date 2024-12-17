3629b1dd66c74ec020d4a5c1aa573e058c16abf0
rafaellichen <rafaellichen@gmail.com>
2024-12-17 14:21:29 -0800

    remove use of router.asPath as it is error prone #368

--- platform/frontend/src/pages/registry/[registryName]/agent/[agentName]/output/new.js
+++ platform/frontend/src/pages/registry/[registryName]/agent/[agentName]/output/new.js
@@ -4,6 +4,7 @@ import NewEntity from "@/components/entity/NewEntity";
 import {
     axiosErrorToast,
     constructSavePropertyRequests,
+    populateRouterPathname,
     settlePromises,
     shallowDiff,
 } from "@/components/helper";
@@ -67,7 +68,7 @@ export default function New() {
     };
     useEffect(() => {
         if (_.isEmpty(router.query)) return;
-        const pathParams = router.asPath
+        const pathParams = populateRouterPathname(router)
             .split("/")
             .filter((param) => !_.isEmpty(param))
             .slice(0, -2);
