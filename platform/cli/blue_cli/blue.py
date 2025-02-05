78181be0ad4cb8fbed61acf2b745129c15ef626d
Eser Kandogan <eser@megagon.ai>
2025-02-05 17:11:34 +0000

    blue_cli

---
+++ platform/cli/blue_cli/blue.py
@@ -0,0 +1,25 @@
+import click
+
+from .commands.profile import ProfileManager, profile
+from .commands.session import session
+from .commands.authentication import Authentication
+import nest_asyncio
+
+nest_asyncio.apply()
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command("login")
+def login():
+    auth = Authentication()
+    cookie = auth.get_cookie()
+    # save cookie under current blue user profile
+    ProfileManager().set_selected_profile_attribute('BLUE_COOKIE', cookie)
+
+
+cli.add_command(profile)
+cli.add_command(session)
