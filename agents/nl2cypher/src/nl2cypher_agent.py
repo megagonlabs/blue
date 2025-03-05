9494cba4585205809bf371502daf47959a18012d
Yanlin <yanlinf23@gmail.com>
2025-03-05 13:14:57 -0800

    Implement an updated Nl2CypherAgent based on NL2SQLAgent

--- agents/nl2cypher/src/nl2cypher_agent.py
+++ agents/nl2cypher/src/nl2cypher_agent.py
@@ -5,95 +5,76 @@ import json
 
 ###### Blue
 from blue.agent import Agent, AgentFactory
-from blue.agents.openai import OpenAIAgent
+from blue.agents.nl2q import NL2SQLAgent
 from blue.session import Session
-from blue.data.registry import DataRegistry
-
 
 # set log level
 logging.getLogger().setLevel(logging.INFO)
-logging.basicConfig(format="%(asctime)s [%(levelname)s] [%(process)d:%(threadName)s:%(thread)d](%(filename)s:%(lineno)d) %(name)s -  %(message)s", level=logging.ERROR, datefmt="%Y-%m-%d %H:%M:%S")
+logging.basicConfig(
+    format="%(asctime)s [%(levelname)s] [%(process)d:%(threadName)s:%(thread)d](%(filename)s:%(lineno)d) %(name)s -  %(message)s",
+    level=logging.ERROR, datefmt="%Y-%m-%d %H:%M:%S")
 
-
-NL2CYPHER_PROMPT = """Your task is to translate a natural language question into a Cypher query based on the schema of a Neo4j knowledge graph.
+NL2CYPHER_PROMPT = """Your task is to translate a natural language question into a Cypher query based on a list of provided data sources.
+For each source you will be provided with the graph schema that specifies the entities, relations and properties.
 
 Here are the requirements:
-- The Cypher query should be compatible with the graph schema. In the graph schema you will be provided with the node properties for each type of node in the graph, the relationship properties for each type of relationship, as well as all unique relationship schemas.
-- Pay attention to the datatypes of the properties in the graph schema.
-- Output the Cyper query directly without ```. Do not generate explanation or other additional output.
+- The output should be a JSON object with the following fields
+  - "question": the original natural language question
+  - "source": the name of the data source that the query will be executed on
+  - "query": the Cypher query that is translated from the natural language question
+- When interpreting the "question" use additional context provided, if available. Ignore information in the context if the question overrides it.
+- The Cypher query should be compatible with the schema of the datasource.
+- Always do case-${sensitivity} matching for string comparison.
+- The query should starts with any of the following prefixes: ${force_query_prefixes}
+- Output the JSON directly. Do not generate explanation or other additional output.
+${additional_requirements}
 
-Graph Schema: ${schema}
+Protocol:
+```
+${protocol}
+```
 
-Question: ${input}
+Data sources:
+```
+${sources}
+```
 
-Cypher Query:
+Context:
+${context}
+
+Question: ${question}
+Output:
 """
 
-# NBA_SCHEMA = """
-# Node properties are the following:
-# Player {name: STRING, date_of_birth: DATE, height_cm: FLOAT, eid: STRING, mass_kg: FLOAT, aliases: LIST, description: STRING},Team {name: STRING, eid: STRING, description: STRING, head_coach: STRING, aliases: LIST},Venue {aliases: LIST, name: STRING, description: STRING, eid: STRING},Position {name: STRING, eid: STRING, description: STRING, aliases: LIST},Award {name: STRING, eid: STRING, aliases: LIST, description: STRING},Division {aliases: LIST, name: STRING, description: STRING, eid: STRING},Conference {aliases: LIST, name: STRING, description: STRING, eid: STRING}
-# Relationship properties are the following:
-# playsFor {end_year: INTEGER, start_year: INTEGER},draftedBy {year: INTEGER},receivesAward {year: INTEGER},hasHomeVenue {end_year: INTEGER, start_year: INTEGER}
-# The relationships are the following:
-# (:Player)-[:playsPosition]->(:Position),(:Player)-[:playsFor]->(:Team),(:Player)-[:draftedBy]->(:Team),(:Player)-[:receivesAward]->(:Award),(:Team)-[:hasHomeVenue]->(:Venue),(:Team)-[:partOfDivision]->(:Division),(:Division)-[:partOfConference]->(:Conference)
-# """
-
-nl2cypher_properties = {
-    "openai.api": "ChatCompletion",
-    "openai.model": "gpt-4o",
-    "output_path": "$.choices[0].message.content",
-    "input_json": "[{\"role\":\"user\"}]",
-    "input_context": "$[0]",
-    "input_context_field": "content",
-    "input_field": "messages",
+agent_properties = {
     "input_template": NL2CYPHER_PROMPT,
-    "openai.temperature": 0,
-    "openai.max_tokens": 512,
-    "listens": {
-        "DEFAULT": {
-            "includes": ["USER"],
-            "excludes": []
-        }
-    }
+    "nl2q_source": None,
+    "nl2q_source_database": None,
+    "nl2q_discovery": True,
+    "nl2q_discovery_similarity_threshold": 0.2,
+    "nl2q_discovery_source_protocols": ["bolt"],
+    "nl2q_execute": True,
+    "nl2q_case_insensitive": True,
+    "nl2q_valid_query_prefixes": ["MATCH"],
+    "nl2q_force_query_prefixes": ["MATCH"]
 }
 
 
-############################
-### OpenAIAgent.Nl2CypherAgent
-#
-class Nl2CypherAgent(OpenAIAgent):
+class Nl2CypherAgent(NL2SQLAgent):
     def __init__(self, **kwargs):
         if 'name' not in kwargs:
             kwargs['name'] = "NL2CYPHER"
         super().__init__(**kwargs)
 
-    def _initialize(self, properties=None):
-        super()._initialize(properties=properties)
-        platform_id = self.properties["platform.name"]
-        prefix = 'PLATFORM:' + platform_id
-        self.registry = DataRegistry(id=self.properties['data_registry.name'], prefix=prefix,
-                                     properties=self.properties)
-
     def _initialize_properties(self):
         super()._initialize_properties()
-        for key in nl2cypher_properties:
-            self.properties[key] = nl2cypher_properties[key]
 
-    def extract_input_params(self, input_data, properties=None):
-        # get properties, overriding with properties provided
-        properties = self.get_properties(properties=properties)
+        # intialize defatult properties
+        for key in agent_properties:
+            self.properties[key] = agent_properties[key]
 
-        neo4j_source = self.registry.connect_source('megagon_hr_insights')
-        schema = neo4j_source.fetch_database_collection_schema('neo4j', 'neo4j')
-        logging.info(type(schema))
-        schema = str(schema)
-        # self.schema = schema
-        return {'schema': schema}
-
-    # def process_output(self, output_data, properties=None):
-    # get properties, overriding with properties provided
-    # properties = self.get_properties(properties=properties)
-    #     return output_data + '<schema>' + self.schema
+    def _format_schema(self, schema):
+        return schema
 
 
 if __name__ == "__main__":
