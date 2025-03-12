3660343e09862afc66fc720fc563d5b65063f475
rafaellichen <rafaellichen@gmail.com>
2025-03-12 11:23:23 -0700

    migrate no FIREBASE_SERVICE_CRED fallback authentication/verification methods

--- platform/api/src/settings.py
+++ platform/api/src/settings.py
@@ -29,6 +29,7 @@ SECURE_COOKIE = os.getenv("BLUE_DEPLOY_SECURE", "True").lower() == "true"
 EMAIL_DOMAIN_WHITE_LIST = os.getenv("BLUE_EMAIL_DOMAIN_WHITE_LIST", "megagon.ai")
 DISABLE_AUTHENTICATION = os.getenv('DISABLE_AUTHENTICATION', 'False').lower() == 'true'
 FIREBASE_SERVICE_CRED = os.getenv("FIREBASE_SERVICE_CRED", "")
+FIREBASE_CLIENT_ID = 'blue-9d597'
 
 ##### RBAC
 import casbin
