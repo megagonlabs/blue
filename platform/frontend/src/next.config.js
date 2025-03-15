f9a4027b5bdee008610e94e5d96dc6aec7234303
rafaellichen <rafaellichen@gmail.com>
2025-03-14 17:06:32 -0700

    fix rewrites not working in production env. due to process.env not available at build time #815

--- platform/frontend/src/next.config.js
+++ platform/frontend/src/next.config.js
@@ -2,17 +2,18 @@ const { configureRuntimeEnv } = require("next-runtime-env/build/configure");
 configureRuntimeEnv();
 module.exports = {
     async rewrites() {
-        const agentRegistryName = process.env.NEXT_PUBLIC_AGENT_REGISTRY_NAME;
-        return [
-            {
-                source: `/registry/${agentRegistryName}/:l1Type(agent|agent_group)/:path(.*\/agent)/:agentName/:l2Type(input|output)/new`,
-                destination: `/registry/${agentRegistryName}/agent/:agentName/:l2Type/new`,
-            },
-            {
-                source: `/registry/${agentRegistryName}/:l1Type(agent|agent_group)/:path(.*\/agent)/new`,
-                destination: `/registry/${agentRegistryName}/agent/new`,
-            },
-        ];
+        return {
+            beforeFiles: [
+                {
+                    source: `/registry/:l0agentRegistryName/:l1Type(agent|agent_group)/:path(.*\/agent)/:agentName/:l2Type(input|output)/new`,
+                    destination: `/registry/:l0agentRegistryName/agent/:agentName/:l2Type/new`,
+                },
+                {
+                    source: `/registry/:l0agentRegistryName/:l1Type(agent|agent_group)/:path(.*\/agent)/new`,
+                    destination: `/registry/:l0agentRegistryName/agent/new`,
+                },
+            ],
+        };
     },
     reactStrictMode: true,
     transpilePackages: ["codemirror-json-schema", "json-schema-library"],
