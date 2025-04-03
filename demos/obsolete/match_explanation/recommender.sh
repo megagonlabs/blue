74c11ad52bd7c2dcf534de5be86cac0e3bd816a6
Eser Kandogan <eser@megagon.ai>
2025-04-03 19:44:56 +0000

    remove demos

--- demos/obsolete/match_explanation/recommender.sh
+++
@@ -1,4 +0,0 @@
-docker rm blue_agent_simple_graph
-# Reommender Agent
-echo "Bring up RECOMMENDER agent:"
-docker run -d --network="host" --name blue_agent_simple_graph blue-agent-simple_graph:latest --session $1 
