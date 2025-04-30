655621dffaeaeb089cdd070c05ec86e31274bc18
rafaellichen <rafaellichen@gmail.com>
2025-04-30 15:06:01 -0700

    add email whitelist #897

--- platform/api/src/routers/accounts.py
+++ platform/api/src/routers/accounts.py
@@ -7,6 +7,7 @@ import json
 import base64
 import time
 import datetime
+from blue.utils.string_utils import encode_websafe_no_padding
 import pydash
 
 ###### FastAPI, Auth, Web
@@ -40,6 +41,12 @@ if not pydash.is_empty(FIREBASE_SERVICE_CRED):
     cred = credentials.Certificate(cert)
     firebase_admin.initialize_app(cred)
 allowed_domains = EMAIL_DOMAIN_WHITE_LIST.split(",")
+
+
+def isEmailInWhitelist(email: str) -> bool:
+    urlsafe_encoded_string = encode_websafe_no_padding(email)
+    result = p.get_metadata(f'settings.allowed_emails.{urlsafe_encoded_string}.allow')
+    return isinstance(result, bool) and result
 
 
 @router.get('/websocket-ticket')
@@ -106,8 +113,8 @@ async def signin(request: Request):
         # }
         email = decoded_claims["email"]
         email_domain = re.search(EMAIL_DOMAIN_ADDRESS_REGEXP, email).group(1)
-        if email_domain not in allowed_domains:
-            return JSONResponse(content={"message": "Invalid email domain"}, status_code=403)
+        if email_domain not in allowed_domains and not isEmailInWhitelist(email):
+            return JSONResponse(content={"message": "Invalid email account"}, status_code=403)
         # Only process if the user signed in within the last 5 minutes.
         if time.time() - decoded_claims["auth_time"] < 5 * 60:
             # Set session expiration to 14 days.
@@ -164,8 +171,8 @@ async def signin_cli(request: Request):
                 return ERROR_RESPONSE
         email = decoded_claims["email"]
         email_domain = re.search(EMAIL_DOMAIN_ADDRESS_REGEXP, email).group(1)
-        if email_domain not in allowed_domains:
-            return JSONResponse(content={"message": "Invalid email domain"}, status_code=403)
+        if email_domain not in allowed_domains and not isEmailInWhitelist(email):
+            return JSONResponse(content={"message": "Invalid email account"}, status_code=403)
         if time.time() - decoded_claims["auth_time"] < 5 * 60:
             expires_in = datetime.timedelta(hours=10)
             if not pydash.is_empty(FIREBASE_SERVICE_CRED):
@@ -203,7 +210,25 @@ def get_profile(request: Request):
     )
 
 
-@router.get('/profile/{uid}')
+@router.get('/profile/email/{email}')
+def get_profile_by_email(request: Request, email):
+    acl_enforce(request.state.user['role'], 'platform_users', 'read_all')
+    user = {}
+    try:
+        if email is not None:
+            if not pydash.is_empty(FIREBASE_SERVICE_CRED):
+                user_record = auth.get_user_by_email(email)
+                user.update({'uid': user_record.uid, 'email': user_record.email, 'picture': user_record.photo_url, 'name': user_record.display_name})
+            else:
+                return JSONResponse(status_code=501, content={"message": 'The server lacks the ability to fulfill the request because environment variable "FIREBASE_SERVICE_CRED" is not configured.'})
+    except auth.UserNotFoundError as ex:
+        return JSONResponse(content={"message": "No user record found for the given identifier."}, status_code=400)
+    except ValueError as ex:
+        print(ex)
+    return JSONResponse(content={"user": user})
+
+
+@router.get('/profile/uid/{uid}')
 def get_profile_by_uid(request: Request, uid):
     acl_enforce(request.state.user['role'], 'platform_users', 'read_all')
     user = {}
@@ -237,8 +262,8 @@ def get_users(request: Request, keyword: str = ""):
             'picture': user['picture'],
         }
         if re.search(rx, user['name']) is not None:
-            # add user role value when querying with admin role
-            if request.state.user['role'] == 'admin':
+            # add user role value when querying with administrator role
+            if request.state.user['role'] == 'administrator':
                 temp['role'] = user['role']
             result.append(temp)
     return JSONResponse(content={"users": result})
