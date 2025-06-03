e9b2d3398a25b0da10a1144ee61c02b162abcbad
Eser Kandogan <eser@ip-10-0-163-164.ec2.internal>
2025-06-03 20:28:35 +0000

    local build

--- operators/openai/docker_build_operator.sh
+++ operators/openai/docker_build_operator.sh
@@ -4,7 +4,7 @@ echo "${BLUE_CORE_DOCKER_ORG}/blue-operator-openai${BLUE_BUILD_IMG_SUFFIX}:${BLU
 echo "plaforms: ${BLUE_BUILD_PLATFORM}"
 
 # build docker
-docker buildx build  --platform ${BLUE_BUILD_PLATFORM} ${BLUE_BUILD_CACHE_ARG} --build-arg BLUE_BUILD_CACHE_ARG --build-arg BLUE_BUILD_LIB_ARG --push -t ${BLUE_DEV_DOCKER_ORG}/blue-operator-openai${BLUE_BUILD_IMG_SUFFIX}:${BLUE_DEPLOY_VERSION} -f Dockerfile.operator .
+docker buildx build  --platform ${BLUE_BUILD_PLATFORM} ${BLUE_BUILD_CACHE_ARG} --build-arg BLUE_BUILD_CACHE_ARG --build-arg BLUE_BUILD_LIB_ARG ${BLUE_BUILD_PUBLISH} -t ${BLUE_DEV_DOCKER_ORG}/blue-operator-openai${BLUE_BUILD_IMG_SUFFIX}:${BLUE_DEPLOY_VERSION} -f Dockerfile.operator .
 
 echo 'Done...'
 
--- platform/setup/docker_build_setup.sh
+++ platform/setup/docker_build_setup.sh
@@ -7,6 +7,6 @@ echo "plaforms: ${BLUE_BUILD_PLATFORM}"
 source $(dirname $0)/build_setup.sh
 
 # build docker
-docker buildx build  --platform ${BLUE_BUILD_PLATFORM} ${BLUE_BUILD_CACHE_ARG} --build-arg BLUE_BUILD_CACHE_ARG --build-arg BLUE_BUILD_LIB_ARG --push -t ${BLUE_CORE_DOCKER_ORG}/blue-platform-setup${BLUE_BUILD_IMG_SUFFIX}:${BLUE_DEPLOY_VERSION} -f Dockerfile.setup .
+docker buildx build  --platform ${BLUE_BUILD_PLATFORM} ${BLUE_BUILD_CACHE_ARG} --build-arg BLUE_BUILD_CACHE_ARG --build-arg BLUE_BUILD_LIB_ARG ${BLUE_BUILD_PUBLISH} -t ${BLUE_CORE_DOCKER_ORG}/blue-platform-setup${BLUE_BUILD_IMG_SUFFIX}:${BLUE_DEPLOY_VERSION} -f Dockerfile.setup .
 
 echo 'Done...'
