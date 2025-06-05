7becea5574daa9a4b0b5fb4c9df34e13561fc2aa
rafaellichen <rafaellichen@gmail.com>
2025-06-05 00:38:00 -0700

    .

--- platform/frontend/next-gen/components/constants.js
+++ platform/frontend/next-gen/components/constants.js
@@ -20,6 +20,7 @@ export const USER_ROLES_LOOKUP = {
         demo: { text: "Demo" },
     },
     TABLE_CELL_HEIGHT = 40,
+    END_OF_EVENT_SIGNAL = "END_OF_EVENT_SIGNAL",
     ENTITY_NAME_SEPARATOR = "___",
     MAIN_INFO_STYLES = {
         height: 40,
