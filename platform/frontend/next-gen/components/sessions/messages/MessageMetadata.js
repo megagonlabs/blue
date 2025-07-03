745029c7a5d86f14358cd1188f2bf7a43ae2be2c
rafaellichen <rafaellichen@gmail.com>
2025-07-03 14:57:12 -0700

    .

--- platform/frontend/next-gen/components/sessions/messages/MessageMetadata.js
+++ platform/frontend/next-gen/components/sessions/messages/MessageMetadata.js
@@ -28,7 +28,7 @@ function MessageMetadata({ message }) {
     const user = useAuthStore((state) => state.user);
     const own = useMemo(() => {
         return isUser && _.isEqual(user.uid, id);
-    }, [user]);
+    }, [user, id]);
     return (
         <div
             style={{
