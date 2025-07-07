0eb918670ce79996e6ba2058a7a1e008851e087d
Eser Kandogan <eser@megagon.ai>
2025-07-07 18:28:33 +0000

    add automatic type signature for local and ray tool servers #980

--- lib/src/blue/tools/clients/ray_tools.py
+++ lib/src/blue/tools/clients/ray_tools.py
@@ -1,50 +1,49 @@
+#### utils, types
+from typing import List
+
 ###### Blue
 from blue.tools.tool import Tool
+from blue.utils import tool_utils
 
-###### Server specific libs
-import ray
 
 tools_dict = {}
 
-### multiply
-@ray.remote
-def add(numbers=None):
+
+### add
+def add(numbers: List[int]) -> int:
     result = 0
     for number in numbers:
         result += number
     return result
 
+
 add_tool = Tool(
-    name = "add",
-    description = "adds numbers and returns the addition as a result",
-    properties = {},
-    function = add,
-    parameters = {
-        "numbers": { "type": "list[Union[int, str]", "required": True}           
-    },
-    validator = lambda params: 'numbers' in params and type(params['numbers']) == list and all([type(number) in [int, float] for number in params['numbers']]),
-    explainer = lambda output, params:  { "output": output, "params": params}
+    name="add",
+    description="adds numbers and returns the addition as a result",
+    properties={},
+    function=add,
+    parameters=tool_utils.extract_signature(add, mcp_format=True)['parameters'],
+    validator=lambda params: 'numbers' in params and type(params['numbers']) == list and all([type(number) in [int, float] for number in params['numbers']]),
+    explainer=lambda output, params: {"output": output, "params": params},
 )
 tools_dict["add"] = add_tool
 
+
 ### multiply
-@ray.remote
-def multiply(numbers=None):
+def multiply(numbers: List[int]) -> int:
     result = 1
     for number in numbers:
         result *= number
     return result
 
+
 multiply_tool = Tool(
-    name = "multiply",
-    description = "multiplies numbers and returns the multiplication ion as a result",
-    properties = {},
-    function = multiply,
-    parameters = {
-        "numbers": { "type": "list[Union[int, str]", "required": True}           
-    },
-    validator = lambda params: 'numbers' in params and type(params['numbers']) == list and all([type(number) in [int, float] for number in params['numbers']]),
-    explainer = lambda output, params: { "output": output, "params": params}
+    name="multiply",
+    description="multiplies numbers and returns the multiplication ion as a result",
+    properties={},
+    function=multiply,
+    parameters=tool_utils.extract_signature(multiply, mcp_format=True)['parameters'],
+    validator=lambda params: 'numbers' in params and type(params['numbers']) == list and all([type(number) in [int, float] for number in params['numbers']]),
+    explainer=lambda output, params: {"output": output, "params": params},
 )
 tools_dict["multiply"] = multiply_tool
-
