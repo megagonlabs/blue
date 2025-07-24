99f097ab77bb62009e117a5e1a0109c4eb7d357f
Eser Kandogan <eser@megagon.ai>
2025-07-24 04:01:25 +0000

    operator json properties organization is incorrect #1034

--- lib/src/blue/tools/tool.py
+++ lib/src/blue/tools/tool.py
@@ -1,13 +1,15 @@
 from dataclasses import dataclass
 from typing import Callable, Dict, Any
 
+
 @dataclass
 class Tool:
-    """A tool is a function, it's parameters, optionally properties, validator to validate input params, and explainer to describe output and potential errors"""
+    """A tool is a function, it's signature, optionally properties, validator to validate input params, and explainer to describe output and potential errors"""
+
     name: str
     description: str
     properties: Dict[str, Any]
     function: Callable[..., Any]
-    parameters: Dict[str, Any]
+    signature: Dict[str, Any]
     validator: Callable[..., Any]
     explainer: Callable[..., Any]
