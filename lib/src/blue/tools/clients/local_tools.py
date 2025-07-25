6a183c263cfc689f0d3389c4ce01ff99f0a5b8ce
Eser Kandogan <eser@megagon.ai>
2025-07-25 03:05:25 +0000

    example set parameter hidden

--- lib/src/blue/tools/clients/local_tools.py
+++ lib/src/blue/tools/clients/local_tools.py
@@ -43,5 +43,5 @@ multiply_tool = Tool(
     explainer=lambda output, params: {"output": output, "params": params},
 )
 # example to hide a parameter
-multiply_tool.properties['signature']['parameters']['x']['hidden'] = True
+multiply_tool.set_parameter_hidden('x', True)
 tools_dict["multiply"] = multiply_tool
