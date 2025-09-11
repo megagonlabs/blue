a593cb6d702eb131c801fb678ddecd257ef0e014
Eser Kandogan <eser@megagon.ai>
2025-09-11 21:12:56 +0000

    progress in multipart query

--- lib/src/blue/operators/multipart_query_operator.py
+++ lib/src/blue/operators/multipart_query_operator.py
@@ -19,9 +19,55 @@ def multipart_query_operator_function(input_data: List[List[Dict[str, Any]]], at
 
 
 def multipart_query_operator_refiner(input_data: List[List[Dict[str, Any]]], attributes: Dict[str, Any], properties: Dict[str, Any] = None) -> List[Dict[str, Any]]:
-    plans = []
 
-    return plans
+    plans = input_data
+    pipelines = []
+
+    for plan in plans:
+        # create pipeline for each alternative set
+        pipeline = DataPipeline(properties=properties)
+
+        ctes = plan
+        # discover, query, create table, and insert for each cte
+        failed = False
+
+        ## build plan
+        # input
+        input_node = pipeline.define_input(value=[[]], properties=properties)
+        # output
+        output_node = pipeline.define_output(properties=properties)
+        for cte in ctes:
+            name = cte['name'] if 'name' in cte else None
+            description = cte['description'] if 'description' in cte else None
+            sql = cte['sql'] if 'sql' in cte else None
+            table = cte['table'] if 'table' in cte else None
+            columns = cte['columns'] if 'columns' in cte else None
+            dependency = cte['dependency'] if 'dependency' in cte else None
+
+            # fail
+            if name is None or description is None or sql is None or table is None or columns is None or dependency is None:
+                failed = True
+                break
+
+            # data discover
+            data_discovery_attributes = {"search_query": description}
+            data_discovery_node = pipeline.define_operator("/server/blue_ray/operator/data_discover", attributes=data_discovery_attributes, properties=properties)
+
+            # nl2q
+            # create table
+            # insert table
+
+            ## connections
+            pipeline.connect_nodes(input_node, data_discovery_node)
+            pipeline.connect_nodes(data_discovery_node, output_node)
+
+        if failed:
+            continue
+
+        # add to pipelines
+        pipelines.append(pipeline.to_dict())
+
+    return pipelines
 
 
 def multipart_query_operator_explainer(output: Any, input_data: List[List[Dict[str, Any]]], attributes: Dict[str, Any]) -> Dict[str, Any]:
