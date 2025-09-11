c81ea6e4616f5e5ff3b36350a6b3c9b52840c1d0
Eser Kandogan <eser@megagon.ai>
2025-09-11 21:11:27 +0000

    get operator attributes thru registry

--- lib/src/blue/operators/clients/ray_client.py
+++ lib/src/blue/operators/clients/ray_client.py
@@ -49,3 +49,15 @@ class RayOperatorClient(RayToolClient, OperatorClient):
             return result
         else:
             return []
+
+    def get_operator_attributes(self, operator):
+        if operator is None:
+            raise Exception("No operator matching...")
+
+        attributes = {}
+
+        if operator in self.tools:
+            operator_obj = self.tools[operator]
+            attributes = operator_obj.get_attributes()
+
+        return attributes
