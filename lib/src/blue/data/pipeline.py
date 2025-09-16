257121e671e19307774a9c5511dfaa4cec94de74
Eser Kandogan <eser@megagon.ai>
2025-09-16 19:43:53 +0000

    add attributes and plan i/o to pipeline

--- lib/src/blue/data/pipeline.py
+++ lib/src/blue/data/pipeline.py
@@ -55,8 +55,76 @@ EntityType.DATA_PIPELINE = EntityType("DATA_PIPELINE")
 ### DataPipeline
 #
 class DataPipeline(dag_utils.Plan):
-    def __init__(self, id=None, label=None, type="DATA_PIPELINE", properties=None, path=None, synchronizer=None, auto_sync=False, sync=None):
+    def __init__(self, id=None, label=None, type="DATA_PIPELINE", properties=None, attributes=None, path=None, plan_input=None, plan_output=None, synchronizer=None, auto_sync=False, sync=None):
         super().__init__(id=id, label=label, type=type, properties=properties, path=path, synchronizer=synchronizer, auto_sync=auto_sync, sync=sync)
+
+        # set plan input / output
+        self.set_plan_input(plan_input, sync=sync)
+        self.set_plan_output(plan_output), sync=sync
+
+        self._initialize_attributes(sync=sync)
+        self._update_attributes(attributes=attributes, sync=sync)
+
+
+    ### attributes
+    def _initialize_attributes(self, sync=None):
+        self.set_data("attributes", {}, sync=sync)
+
+    def _update_attributes(self, attributes=None, sync=None):
+        if attributes is None:
+            return
+
+        # override
+        for a in attributes:
+            self.set_attribute(a, attributes[a], sync=sync)
+
+    def set_attribute(self, key, value, sync=None):
+        attributes = self.get_attributes()
+        attributes[key] = value
+
+        self.synchronize(key="attributes." + key, value=value)
+
+    def get_attribute(self, key):
+        attributes = self.get_attributes()
+        if key in attributes:
+            return attributes[key]
+        return None
+
+    def get_attributes(self):
+        return self.get_data("attributes")
+    
+    # plan input / output
+    def set_plan_input_id(self, input_id, sync=None):
+        self.set_plan_input(input_id, sync=sync)
+
+    def set_plan_input(self, i, sync=None):
+        input_id = i
+
+        input_node = self.get_node(i)
+        # internal node, get node id
+        if input_node:
+            input_id = input_node.get_id()
+
+        self.set_data("input", input_id, sync=sync)
+
+    def get_plan_input_id(self):
+        return self.get_data("input")
+    
+    def set_plan_output_id(self, output_id, sync=None):
+        self.set_plan_output(output_id, sync=sync)
+
+    def set_plan_output(self, o, sync=None):
+        output_id = o
+
+        output_node = self.get_node(o)
+        # internal node, get node id
+        if output_node:
+            output_id = output_node.get_id()
+
+        self.set_data("output", output_id, sync=sync)
+
+    def get_plan_output_id(self):
+        return self.get_data("output")
 
     # nodes
     def define_input(self, label=None, value=None, properties={}, sync=None):
