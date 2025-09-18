20df83eca737239ab13f6a7cb1021e177c073064
Eser Kandogan <eser@megagon.ai>
2025-09-18 16:25:50 +0000

    add create_table to multipart query operator

--- lib/src/blue/operators/multipart_query_operator.py
+++ lib/src/blue/operators/multipart_query_operator.py
@@ -76,19 +76,19 @@ def multipart_query_operator_refiner(input_data: List[List[Dict[str, Any]]], att
             data_discovery_node = pipeline.define_operator("/server/blue_ray/operator/data_discover", attributes=data_discovery_attributes, properties={})
 
             # create table
-            # create_table_attributes = {"source": "internal", "database": db_name, "table": table, "columns": columns}
-            # create_table_node = pipeline.define_operator("/server/blue_ray/operator/create_table", attributes=create_table_attributes, properties={})
+            create_table_attributes = {"source": "internal", "database": db_name, "table": table, "columns": columns}
+            create_table_node = pipeline.define_operator("/server/blue_ray/operator/create_table", attributes=create_table_attributes, properties={})
 
             # nl2q
             nl2query_router_attributes = {"search_query": description, "execute_query": True, "columns": columns}
-            nnl2query_router_node = pipeline.define_operator("/server/blue_ray/operator/nl2query_router", attributes=nl2query_router_attributes, properties={})
+            nl2query_router_node = pipeline.define_operator("/server/blue_ray/operator/nl2query_router", attributes=nl2query_router_attributes, properties={})
 
             # # insert table
             # insert_table_attributes = {"source": "internal", "database": db_name, "collection": table}
             # it_node = pipeline.define_operator("/server/blue_ray/operator/insert_table", attributes=insert_table_attributes, properties={})
 
             start_node = data_discovery_node
-            end_node = nnl2query_router_node  # TODO: modify this
+            end_node = nl2query_router_node  # TODO: modify this
 
             ## set cte start / end nodes
             dependents.add(name)
@@ -96,7 +96,8 @@ def multipart_query_operator_refiner(input_data: List[List[Dict[str, Any]]], att
             cte_end_nodes[name] = end_node
 
             ## intra-cte connections
-            pipeline.connect_nodes(data_discovery_node, nnl2query_router_node)
+            pipeline.connect_nodes(data_discovery_node, create_table_node)
+            pipeline.connect_nodes(create_table_node, nl2query_router_node)
 
             # remove any dependency
             for d in dependency:
