e3a4310451baa2b16e410629560ae185fea76c95
jalalmahmud <jalal.mahmud@gmail.com>
2025-09-18 18:32:22 +0000

    default collection is public for sqlite

--- lib/src/blue/data/sources/sqlite_source.py
+++ lib/src/blue/data/sources/sqlite_source.py
@@ -123,9 +123,9 @@ class SQLiteDBSource(DataSource):
 
     ######### database/collection
     def fetch_database_collections(self, database):
-        ## for sqlite, collection is the database, we are returning the database
+        ## for sqlite, there is no collection actually. We are returning "public" to have data registry entry
         collections = []
-        collections.append(database)
+        collections.append("public")
         return collections 
         
     def fetch_database_collection_metadata(self, database, collection):
