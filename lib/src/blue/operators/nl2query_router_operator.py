666ce7ba7d496356ac0be1e4388b3ff742d1c3d1
Eser Kandogan <eser@megagon.ai>
2025-09-21 17:26:17 +0000

    fix typo)

--- lib/src/blue/operators/nl2query_router_operator.py
+++ lib/src/blue/operators/nl2query_router_operator.py
@@ -49,8 +49,8 @@ def nl2query_router_operator_refiner(input_data: List[List[Dict[str, Any]]], att
         protocol = connection['protocol']
 
         # input
-        input_node = pipeline.define_input(value=None
-        
+        input_node = pipeline.define_input(value=None)
+
         # output
         output_node = pipeline.define_output(properties={})
 
