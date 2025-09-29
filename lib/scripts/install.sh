853b580701b19b39033d4c13009c356a48014985
Eser Kandogan <eser@megagon.ai>
2025-09-29 18:36:54 +0000

    install/uninstall libs/cli dynamic version

--- lib/scripts/install.sh
+++ lib/scripts/install.sh
@@ -2,4 +2,4 @@
 echo 'Installing blue lib...'
 
 # install
-pip install ${BLUE_BUILD_CACHE_ARG} ${BLUE_BUILD_LIB_ARG} blue-platform
+pip install ${BLUE_BUILD_CACHE_ARG} ${BLUE_BUILD_LIB_ARG} blue-platform==${BLUE_DEPLOY_VERSION}
--- platform/cli/scripts/install.sh
+++ platform/cli/scripts/install.sh
@@ -2,4 +2,4 @@
 echo 'Installing blue cli lib...'
 
 # install
-pip install ${BLUE_BUILD_CACHE_ARG} ${BLUE_BUILD_LIB_ARG} blue-cli
+pip install ${BLUE_BUILD_CACHE_ARG} ${BLUE_BUILD_LIB_ARG} blue-cli==${BLUE_DEPLOY_VERSION}
