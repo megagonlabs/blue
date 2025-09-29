ec26277c03569484d4c36cf709b75452ec392a7c
jalalmahmud <jalal.mahmud@gmail.com>
2025-09-29 21:52:11 +0000

    data registry cli changes, it will need more work w.r.t authentication before we can release

--- platform/cli/src/blue_cli/blue.py
+++ platform/cli/src/blue_cli/blue.py
@@ -5,16 +5,25 @@ from .commands.profile import  profile
 # from .commands.session import session
 from .commands.platform import platform
 from .commands.service import service
+from .commands.registry import registry
+
 import nest_asyncio
 
 nest_asyncio.apply()
 
 
+#@click.group()
+#def cli():
+#    pass
+
+
 @click.group()
-def cli():
-    pass
+@click.pass_context
+def cli(ctx):
+    ctx.ensure_object(dict)  # context for subcommands
 
 cli.add_command(profile)
 cli.add_command(platform)
 cli.add_command(service)
 # cli.add_command(session)
+cli.add_command(registry)
