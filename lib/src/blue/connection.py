99bd4fde3227341a54089c48a029f10eb657b17b
jalalmahmud <jalal.mahmud@gmail.com>
2025-10-08 07:16:35 +0000

    solved vector access with no_decode connection using instance level connection from connection pool, this commit solves ineffiency noted in the bug issue #1262

--- lib/src/blue/connection.py
+++ lib/src/blue/connection.py
@@ -15,8 +15,12 @@ class PooledConnectionFactory:
     __pool = None
     __pool_id = None
 
-    def __init__(self, properties=None):
+    def __init__(self, properties=None, decode_responses=True, use_instance_pool=False):
         self.properties = properties
+
+        self.use_instance_pool = use_instance_pool
+        self.decode_responses = decode_responses
+        self._instance_pool = None
 
         # start connection pool
         self._start()
@@ -34,33 +38,38 @@ class PooledConnectionFactory:
         if "db.max_connections" in self.properties:
             max_connections = self.properties["db.max_connections"]
 
-        # decoding
-        decode_responses = True
-        if "db.decode_responses" in self.properties:
-            decode_responses = self.properties["db.decode_responses"]
-
-        # init class connection
-        if PooledConnectionFactory.__pool is None:
-            PooledConnectionFactory.__pool_id = uuid_utils.create_uuid()
-            PooledConnectionFactory.__pool = redis.connection.ConnectionPool(host=host, port=port, max_connections=max_connections, decode_responses=decode_responses)
-
+        if self.use_instance_pool:
+            self._instance_pool = redis.ConnectionPool(
+                host=host, port=port, max_connections=max_connections, decode_responses=self.decode_responses
+            )
+        else:
+            if PooledConnectionFactory.__pool is None:
+                PooledConnectionFactory.__pool_id = uuid_utils.create_uuid()
+                PooledConnectionFactory.__pool = redis.ConnectionPool(
+                    host=host, port=port, max_connections=max_connections, decode_responses=self.decode_responses
+                )
+    
     def get_id(self):
-        return PooledConnectionFactory.__pool_id
+        return PooledConnectionFactory.__pool_id if not self.use_instance_pool else None
 
     def get_connection(self):
         if self.connection is None:
-            self.connection = redis.Redis(connection_pool=PooledConnectionFactory.__pool)
-
+            pool = self._instance_pool if self.use_instance_pool else PooledConnectionFactory.__pool
+            self.connection = redis.Redis(connection_pool=pool)
+          
         return self.connection
 
     def count_in_use_connections(self):
-        return len(PooledConnectionFactory.__pool._in_use_connections)
+        pool = self._instance_pool if self.use_instance_pool else PooledConnectionFactory.__pool
+        return len(pool._in_use_connections)
 
     def count_created_connections(self):
-        return PooledConnectionFactory.__pool._created_connections
-
+        pool = self._instance_pool if self.use_instance_pool else PooledConnectionFactory.__pool
+        return pool._created_connections
+        
     def count_available_connections(self):
-        return len(PooledConnectionFactory.__pool._available_connections)
-
+        pool = self._instance_pool if self.use_instance_pool else PooledConnectionFactory.__pool
+        return len(pool._available_connections)
+        
     def __repr__(self):
         return f"PooledConnectionManager(connection={self.connection}, pool_info=(created={self.count_created_connections()}, in_use={self.count_in_use_connections()}, available={self.count_available_connections()}))"
