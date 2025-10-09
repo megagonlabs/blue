ec1939a231929302a2867e330638d5ea7b4a0990
jalalmahmud <jalal.mahmud@gmail.com>
2025-10-09 01:16:27 +0000

    added docstring for schema.py

--- lib/src/blue/data/schema.py
+++ lib/src/blue/data/schema.py
@@ -3,14 +3,34 @@
 #
 class DataSchema():
     def __init__(self):
-
+        """Initialize empty containers for entities and relations."""
         self.entities = {}
         self.relations = {}
 
     def has_entity(self, key):
+        """
+        Check if an entity exists in the schema.
+
+        Args:
+            key (str): The entity name or identifier.
+
+        Returns:
+            bool: True if the entity exists, False otherwise.
+        """
         return key in self.entities
 
     def add_entity(self, key):
+        """
+        Add a new entity to the schema.
+
+        If the entity already exists, a unique suffix is appended (e.g., "__1").
+
+        Args:
+            key (str): The name of the entity.
+
+        Returns:
+            str: The original entity key.
+        """
         index = 0
         if key in self.entities:
             entity_obj = self.entities[key]
@@ -38,6 +58,14 @@ class DataSchema():
         return key
 
     def add_entity_property(self, key, attribute, type):
+        """
+        Add a property (attribute) to an existing entity.
+
+        Args:
+            key (str): The entity key.
+            attribute (str): The name of the attribute.
+            type (str): The type or description of the attribute.
+        """
         if key not in self.entities:
             return
         entity_obj = self.entities[key]
@@ -51,15 +79,50 @@ class DataSchema():
         
     
     def _relation_encoding(self, source, relation, target):
+        """
+        Create a unique encoded key for a relation.
+
+        Args:
+            source (str): The source entity name.
+            relation (str): The relationship type.
+            target (str): The target entity name.
+
+        Returns:
+            str: Encoded relation identifier string.
+        """
         s = source + " " + relation + " " + target 
         return s.replace(" ", "__")
         # return "(" + source + ")" + "-" + relation + "->" + "(" + target + ")" 
 
     def has_relation(self, source, relation, target):
+        """
+        Check if a relation exists in the schema.
+
+        Args:
+            source (str): Source entity name.
+            relation (str): Relation name.
+            target (str): Target entity name.
+
+        Returns:
+            bool: True if the relation exists, False otherwise.
+        """
         relation_encoding = self._relation_encoding(source, relation, target)
         return relation_encoding in self.relations 
 
     def add_relation(self, source, relation, target):  
+        """
+        Add a relation between two entities in the schema.
+
+        If the same relation already exists, a numeric suffix is appended.
+
+        Args:
+            source (str): Source entity name.
+            relation (str): Relation name.
+            target (str): Target entity name.
+
+        Returns:
+            str: Unique key of the created relation.
+        """
         key = self._relation_encoding(source, relation, target)
         index = 0
         if key in self.relations:
@@ -83,18 +146,44 @@ class DataSchema():
         return unique_key
 
     def add_relation_property(self, key, property, type):
+        """
+        Add a property to an existing relation.
+
+        Args:
+            key (str): Relation key.
+            property (str): Property name.
+            type (str): Property type or description.
+        """
         if key in self.relations:
             relation_obj = self.relations[key]
             properties_obj = relation_obj['properties']
             properties_obj[property] = type
 
     def get_entities(self):
+        """
+        Get all entities defined in the schema.
+
+        Returns:
+            dict: Mapping of entity keys to entity definitions.
+        """
         return self.entities
 
     def get_relations(self):
+        """
+        Get all relations defined in the schema.
+
+        Returns:
+            dict: Mapping of relation keys to relation definitions.
+        """
         return self.relations
 
     def to_json(self):
+        """
+        Convert the schema to a JSON-serializable dictionary.
+
+        Returns:
+            dict: Dictionary with 'entities' and 'relations' keys.
+        """
         s = {}
         s['entities'] = self.entities.copy()
         s['relations'] = self.relations.copy()
