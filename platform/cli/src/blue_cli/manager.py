2fb2fc42b319a97cf511641d17fcafe1001e30ab
Eser Kandogan <eser@megagon.ai>
2025-10-15 17:32:08 +0000

    ray

--- platform/cli/src/blue_cli/manager.py
+++ platform/cli/src/blue_cli/manager.py
@@ -917,7 +917,7 @@ class PlatformManager:
             labels={"blue.platform": BLUE_DEPLOY_PLATFORM + "." + "ray"},
             environment=config,
             restart_policy={"Name": "always"},
-            shm_size="5.04gb",
+            shm_size="4G",
             detach=True,
             stdout=True,
             stderr=True,
