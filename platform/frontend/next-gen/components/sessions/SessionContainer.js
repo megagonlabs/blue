3d1ddad4cffb3cb5a50e00f4950ed0058d3dcb3f
rafaellichen <rafaellichen@gmail.com>
2025-12-03 11:42:41 -0800

    fix ui issue: split pane resize is draggable when add agent dialog is showing

--- platform/frontend/next-gen/components/sessions/SessionContainer.js
+++ platform/frontend/next-gen/components/sessions/SessionContainer.js
@@ -120,6 +120,7 @@ function SessionContainer({ width, height, sessionId }) {
                 }}
             >
                 <Overlay2
+                    backdropProps={{ style: { zIndex: 36 } }}
                     onClose={() => {
                         setShowAddSessionAgent(false);
                         setSkippable(false);
@@ -132,6 +133,7 @@ function SessionContainer({ width, height, sessionId }) {
                     <div
                         className="custom-card center-center"
                         style={{
+                            zIndex: 36,
                             width: 650,
                             padding: 20,
                             height: "calc(100% - 40px)",
