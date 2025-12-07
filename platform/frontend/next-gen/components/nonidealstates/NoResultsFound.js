90931af71a9eee097b3a0526a52e1cc189b315e0
rafaellichen <rafaellichen@gmail.com>
2025-12-07 12:31:50 -0800

    update noresultsfound icon

--- platform/frontend/next-gen/components/nonidealstates/NoResultsFound.js
+++ platform/frontend/next-gen/components/nonidealstates/NoResultsFound.js
@@ -1,12 +1,12 @@
 import { NonIdealState } from "@blueprintjs/core";
-import { faShelvesEmpty } from "@fortawesome/sharp-duotone-solid-svg-icons";
+import { faCartFlatbedEmpty } from "@fortawesome/sharp-duotone-solid-svg-icons";
 import { FAIcon } from "../FAIcon";
 export default function NoResultsFound() {
     return (
         <NonIdealState
             title="No results found"
             description="It seems we can't find any results based on your search."
-            icon={<FAIcon size={50} icon={faShelvesEmpty} />}
+            icon={<FAIcon size={50} icon={faCartFlatbedEmpty} />}
         />
     );
 }
