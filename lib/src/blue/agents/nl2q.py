274bdfde1334c55b8ed026c4ff6e70bc072c0355
rafaellichen <rafaellichen@gmail.com>
2025-12-09 14:18:50 -0800

    continue fix for nl2q involving attempts to loop on None #1364

--- lib/src/blue/agents/nl2q.py
+++ lib/src/blue/agents/nl2q.py
@@ -1,6 +1,7 @@
 ###### Parsers, Formats, Utils
 import logging
 import json
+from collections.abc import Iterable
 
 
 ###### Blue
@@ -235,8 +236,9 @@ Output:
                             entity_dict = self.registry.get_source_database_collection_entity(source, database, collection, entity)
                             if entity_dict is not None:
                                 # Initialize attributes list from contents if attribute=None
-                                if attribute is None and 'contents' in entity_dict and 'attribute' in entity_dict['contents']:
-                                    entity_dict['attributes'] = list(entity_dict['contents']['attribute'].values())
+                                if attribute is None and 'contents' in entity_dict:
+                                    if isinstance(entity_dict['contents'], Iterable) and 'attribute' in entity_dict['contents']:
+                                        entity_dict['attributes'] = list(entity_dict['contents']['attribute'].values())
                                 else:
                                     entity_dict['attributes'] = []
 
@@ -250,7 +252,7 @@ Output:
                             attribute_dict = self.registry.get_source_database_collection_entity_attribute(source, database, collection, entity, attribute)
 
                             if entity_dict:
-                                if isinstance(entity_dict['attributes'], list) and all(attr['name'] != attribute_dict['name'] for attr in entity_dict['attributes']):
+                                if isinstance(entity_dict['attributes'], Iterable) and all(attr['name'] != attribute_dict['name'] for attr in entity_dict['attributes']):
                                     entity_dict['attributes'].append(attribute_dict)
 
                     if relation:
