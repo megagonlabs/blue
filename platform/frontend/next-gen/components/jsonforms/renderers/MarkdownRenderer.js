f14021c1c1f8acab4dcfbab172998d376b5285ef
rafaellichen <rafaellichen@gmail.com>
2025-12-10 14:43:05 -0800

    fix jsonforms renderers styles not all using convertCss

--- platform/frontend/next-gen/components/jsonforms/renderers/MarkdownRenderer.js
+++ platform/frontend/next-gen/components/jsonforms/renderers/MarkdownRenderer.js
@@ -1,9 +1,10 @@
+import { convertCss } from "@/components/helper";
 import Markdown from "@/components/sessions/messages/renderers/Markdown";
 import { rankWith, uiTypeIs } from "@jsonforms/core";
 import { withJsonFormsControlProps } from "@jsonforms/react";
 import _ from "lodash";
 const MarkdownRenderer = ({ uischema, data }) => {
-    const style = _.get(uischema, "props.style", {});
+    const style = convertCss(_.get(uischema, "props.style", {}));
     return (
         <div style={{ overflow: "auto" }}>
             <Markdown content={data} style={style} />
